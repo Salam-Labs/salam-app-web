9b4e5ebf373e23acfddbcf30395b7988ea251c9b
Golam <74001687+golam71@users.noreply.github.com>
2022-10-30 14:24:23 +0600

    Update Testimonials.jsx

--- src/solid/Testimonials.jsx
+++ src/solid/Testimonials.jsx
@@ -7,8 +7,7 @@ const testimonials = [
         "TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.",
       author: {
         name: "Sheryl Berge",
-        role: "CEO at Lynch LLC",
-        image: avatarImage1,
+
       },
     },
     {
@@ -16,8 +15,7 @@ const testimonials = [
         "I’m trying to get a hold of someone in support, I’m in a lot of trouble right now and they are saying it has something to do with my books. Please get back to me right away.",
       author: {
         name: "Amy Hahn",
-        role: "Director at Velocity Industries",
-        image: avatarImage1,
+
       },
     },
   ],
@@ -27,8 +25,7 @@ const testimonials = [
         "The best part about TaxPal is every time I pay my employees, my bank balance doesn’t go down like it used to. Looking forward to spending this extra cash when I figure out why my card is being declined.",
       author: {
         name: "Leland Kiehn",
-        role: "Founder of Kiehn and Sons",
-        image: avatarImage1,
+
       },
     },
     {
@@ -36,8 +33,7 @@ const testimonials = [
         "There are so many things I had to do with my old software that I just don’t do at all with TaxPal. Suspicious but I can’t say I don’t love it.",
       author: {
         name: "Erin Powlowski",
-        role: "COO at Armstrong Inc",
-        image: avatarImage1,
+
       },
     },
   ],
@@ -47,8 +43,7 @@ const testimonials = [
         "I used to have to remit tax to the EU and with TaxPal I somehow don’t have to do that anymore. Nervous to travel there now though.",
       author: {
         name: "Peter Renolds",
-        role: "Founder of West Inc",
-        image: avatarImage1,
+
       },
     },
     {
@@ -56,8 +51,7 @@ const testimonials = [
         "This is the fourth email I’ve sent to your support team. I am literally being held in jail for tax fraud. Please answer your damn emails, this is important.",
       author: {
         name: "Amy Hahn",
-        role: "Director at Velocity Industries",
-        image: avatarImage1,
+
       },
     },
   ],
@@ -76,10 +70,10 @@ export function Testimonials() {
             id="testimonials-title"
             className="font-display font-bold text-3xl tracking-tight text-white sm:text-4xl"
           >
-            Loved by businesses worldwide.
+            Loved by people worldwide.
           </h2>
           <p className="mt-4 text-white text-lg tracking-tight text-slate-700">
-            Our software is so simple that people can’t help but fall in love
+            Salam App is so simple that people can’t help but fall in love
             with it. Simplicity is easy when you just skip tons of
             mission-critical features.
           </p>
@@ -104,18 +98,13 @@ export function Testimonials() {
                           {testimonial.content}
                         </p>
                       </blockquote>
-                      <figcaption className="relative mt-6 flex items-center justify-between border-t border-slate-100 pt-6">
+                      <figcaption className="relative mt-6 flex items-center flex-row-reverse text-left justify-between border-t border-slate-100 pt-6">
                         <div>
                           <div className="font-display text-base text-white">
                             {testimonial.author.name}
                           </div>
-                          <div className="mt-1 text-sm text-grey-50">
-                            {testimonial.author.role}
-                          </div>
                         </div>
-                        <div className="h-14 w-14 overflow-hidden rounded-full bg-slate-50">
-                          <img src={testimonial.author.image} alt="" />
-                        </div>
+
                       </figcaption>
                     </figure>
                   </li>
