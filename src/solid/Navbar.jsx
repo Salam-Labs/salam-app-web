361c94a12166cbe45dac14d676a4f353b3768939
Golam Moula <74001687+golam71@users.noreply.github.com>
2023-11-14 18:22:20 +0000

    l

--- src/solid/Navbar.jsx
+++ src/solid/Navbar.jsx
@@ -153,7 +153,7 @@ export const NavBar = (props) => {
               href="/team"
               class="text-gray-300  hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
-              Team
+              Team  
             </a>
 
 
