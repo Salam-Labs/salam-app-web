b644daca7e833abab49eeee28372957a82291261
golam71 <mrgenius408@gmail.com>
2024-01-05 19:38:14 +0600

    fix critical

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
-import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless"; // Updated import
 
 export default defineConfig({
   site: "https://salam.app",
-  integrations: [tailwind(), sitemap(), solidJs(), compress(), icon()],
+  integrations: [tailwind(), sitemap(), solidJs(), compress()],
   vite: {
     ssr: {
       noExternal: ["solid-dismiss"],
